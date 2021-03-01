ba9ce7f4f18f8b1e58273633bb9bf7118276496d
Stephanie300 <60020856+Stephanie300@users.noreply.github.com>
2021-02-28 22:06:15 -0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Assignment 2
+
+Employee Manager Site
+
+to run project: npm run server
+                npm run start
+
+
+ 
+
+
+ 
+  
---
+++ server/services/registerService.js
@@ -0,0 +1,46 @@
+const fileService = require('./fileService')
+
+exports.authenticate = (credential)=>{
+ 
+    const {fullname, email, password} = {...credential}
+    const users = fileService.getFileContents('../data/users.json');
+
+    const authUser =  users.reduce((authObj, user)=>{  
+     
+        if(user.email === email){
+          authObj.validEmail = true;
+        }else{
+           // errorObj
+        }
+    
+        if(user.password === password){
+          authObj.validPassword = true;
+        }else{
+          // passwordError authObj.passwordError = "something"
+        }
+    
+        if(authObj.validEmail===true && authObj.validPassword===true){
+            authObj.user = user;
+        }
+             
+        return authObj
+    
+       }, {validEmail:false, validPassword:false, user:null})
+    
+        // ternary opertoar   ()?true:false
+        // if() else
+        // truthy falsy
+       const auth0 = authUser.user ? {user:authUser.user}: formatErrors(authUser);
+       return auth0
+    
+    }
+     
+    const formatErrors = function(user){
+      let passwordWarning = ""
+      let emailWarning = ""
+    
+      if(user.validPassword === false){passwordWarning= `password doesn't seem to be correct`}
+      if(user.validEmail === false){emailWarning= `email doesn't seem to be correct`}
+    
+      return {user:null, emailWarning, passwordWarning}
+    }
